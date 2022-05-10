8148139b2b4f8a9b495a76fe0fe320ea27a4a38f
DevBalek <muhammedfurkanbalek@gmail.com>
2022-05-10 20:20:51 +0300

    changed notify

--- src/Observer/IPublisher.java
+++ src/Observer/IPublisher.java
@@ -5,6 +5,6 @@ import islemler.IEyleyici;
 public interface IPublisher {
     void attach(IObserver observer);
     void detach(IObserver observer);
-    void acilDurumBildirimi(String mesaj,String level,IEyleyici eyleyici) throws InterruptedException;
+    void notify(String mesaj,String level,IEyleyici eyleyici) throws InterruptedException;
 
 }
